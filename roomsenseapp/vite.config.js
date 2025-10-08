4738e51dc9207b6b2f1e98feacc6cf6cf32f7028
Eray <90530537+Vondalo@users.noreply.github.com>
2025-10-08 18:33:40 +0200

    HTTPS

--- roomsenseapp/vite.config.js
+++ roomsenseapp/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./certificates/cert.key'),
+      cert: fs.readFileSync('./certificates/cert.crt'),
+    },
+    host: true, // Allow external connections
+  },
 })
