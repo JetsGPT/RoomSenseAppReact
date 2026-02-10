0ffff92a4e7e548e145825a7cea4e1ef8b5d6ca1
Eray <90530537+Vondalo@users.noreply.github.com>
2026-02-10 18:51:15 +0100

    fix merge conflict

--- roomsenseapp/vite.config.js
+++ roomsenseapp/vite.config.js
@@ -3,17 +3,19 @@ import { fileURLToPath } from "url"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-import basicSsl from '@vitejs/plugin-basic-ssl'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), basicSsl()],
   server: {
+    https: true,
+    host: true,
     proxy: {
       '/api': {
         target: 'https://localhost:8081',
